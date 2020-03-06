253656d084207dee7e45c1bd2f025739c893721e
danielphilipjohnson <daniel-philip-johnson@outlook.com>
2020-03-06 14:45:14 +0000

    Remove duplicate files

--- intro-cs/python-for-everyone/exercise-3.py
+++
@@ -1,4 +0,0 @@
-hours = float(input("Enter Hours: "))
-rate_per_hour = float(input("Enter Rate: "))
-pay = hours * rate_per_hour
-print("Pay: ", pay)
--- intro-cs/python-for-everyone/exercise-5.py
+++
@@ -1,4 +0,0 @@
-c = float(input("Give a  temperature in celsius: "))
-f =  (c * 9/5) + 32
-
-print(f)
