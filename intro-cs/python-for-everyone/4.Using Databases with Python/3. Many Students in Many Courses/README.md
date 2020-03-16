ddc555a6b52b1f8bcd543b46dd5683aaa37b819a
danielphilipjohnson <daniel-philip-johnson@outlook.com>
2020-03-16 13:24:51 +0000

    Add role to members table

--- intro-cs/python-for-everyone/4.Using Databases with Python/3. Many Students in Many Courses/README.md
+++ intro-cs/python-for-everyone/4.Using Databases with Python/3. Many Students in Many Courses/README.md
@@ -2,3 +2,6 @@
 
 This assignment will be a Python program to build a set of tables using the Many-to-Many approach to store enrollment and role data.
 
+Instructions
+
+This application will read roster data in JSON format, parse the file, and then produce an SQLite database that contains a User, Course, and Member table and populate the tables from the data file. 
--- intro-cs/python-for-everyone/4.Using Databases with Python/3. Many Students in Many Courses/roster.py
+++ intro-cs/python-for-everyone/4.Using Databases with Python/3. Many Students in Many Courses/roster.py
@@ -3,7 +3,12 @@ import sqlite3
 
 conn = sqlite3.connect('rosterdb.sqlite')
 cur = conn.cursor()
-
+"""
+TODO
+this code is incomplete as you need to modify the program to store the role column in the Member table to complete the assignment.
+                        member
+#   [ "Charley", "si110", 1 ],
+"""
 # Do some setup
 cur.executescript('''
 DROP TABLE IF EXISTS User;
@@ -30,7 +35,7 @@ CREATE TABLE Member (
 
 fname = input('Enter file name: ')
 if len(fname) < 1:
-    fname = 'roster_data_sample.json'
+    fname = 'roster_data.json'
 
 # [
 #   [ "Charley", "si110", 1 ],
@@ -41,11 +46,10 @@ json_data = json.loads(str_data)
 
 for entry in json_data:
 
-    name = entry[0];
-    title = entry[1];
-
-    print((name, title))
-
+    name = entry[0]
+    title = entry[1]
+    role =  entry[2]
+    print((name, title, role))
     cur.execute('''INSERT OR IGNORE INTO User (name)
         VALUES ( ? )''', ( name, ) )
     cur.execute('SELECT id FROM User WHERE name = ? ', (name, ))
@@ -57,7 +61,7 @@ for entry in json_data:
     course_id = cur.fetchone()[0]
 
     cur.execute('''INSERT OR REPLACE INTO Member
-        (user_id, course_id) VALUES ( ?, ? )''',
-        ( user_id, course_id ) )
+        (user_id, course_id, role) VALUES ( ?, ?, ? )''',
+        ( user_id, course_id, role ) )
 
     conn.commit()
