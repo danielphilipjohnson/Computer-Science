4ccb829f663e7244ea63e736d3ad274b2c72ce6e
danielphilipjohnson <daniel-philip-johnson@outlook.com>
2020-06-02 18:52:19 +0100

    add processed images

---
+++ Web-Technologies/web-for-everyone/5.web-design-capstone/6.week/lib/calculator.js
@@ -0,0 +1,19 @@
+var calc = (function() {
+
+    function add(x, y) {
+        return x + y
+
+
+    }
+    return {
+        add: add
+    }
+})();
+
+
+
+function add(x, y) {
+    return x + y
+
+
+}
