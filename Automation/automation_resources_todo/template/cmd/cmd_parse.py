1653e1cda63825de205b04c42bd50b9d1598a73d
danielphilipjohnson <daniel-philip-johnson@outlook.com>
2020-06-02 20:59:04 +0100

    Restructured automation

---
+++ Automation/automation_resources_todo/template/cmd/cmd_parse.py
@@ -0,0 +1,14 @@
+import argparse
+
+def main(number):
+    print('#' * number)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('number', type=int, help='A number')
+    args = parser.parse_args()
+    main(args.number)
+
+# usage
+# python3 cmd_parse.py 100
+
