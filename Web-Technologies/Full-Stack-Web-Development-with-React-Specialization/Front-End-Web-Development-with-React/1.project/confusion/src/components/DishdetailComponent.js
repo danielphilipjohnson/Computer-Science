28b0e547923e83dc70b050384dd4af15f04e2063
danielphilipjohnson <daniel-philip-johnson@outlook.com>
2020-07-08 12:11:40 +0100

    Components part 2

---
+++ Web-Technologies/Full-Stack-Web-Development-with-React-Specialization/Front-End-Web-Development-with-React/1.project/confusion/src/components/DishdetailComponent.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import { Card } from 'reactstrap';
+
+export default class DishdetailComponent extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
