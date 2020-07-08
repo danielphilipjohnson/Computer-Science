ec5ed47574d6bf9b5a402614bbecaecbb277e5f8
danielphilipjohnson <daniel-philip-johnson@outlook.com>
2020-07-08 15:32:40 +0100

    Single Page Applications Part 2

--- Web-Technologies/Full-Stack-Web-Development-with-React-Specialization/Front-End-Web-Development-with-React/1.project/confusion/src/components/DishdetailComponent.js
+++ Web-Technologies/Full-Stack-Web-Development-with-React-Specialization/Front-End-Web-Development-with-React/1.project/confusion/src/components/DishdetailComponent.js
@@ -1,11 +1,7 @@
 import React, {Component} from 'react'
-import {
-    Card,
-    CardImg,
-    CardText,
-    CardBody,
-    CardTitle
-} from 'reactstrap';
+import { Card, CardImg, CardText, CardBody,
+    CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
 
 
@@ -79,19 +75,30 @@ function RenderComments({comments}) {
 }
 
 const  DishDetail = (props) => {
+    
     return (
+        <div className="container">
+        <div className="row">
+            <Breadcrumb>
+
+                <BreadcrumbItem><Link to="/menu">Menu</Link></BreadcrumbItem>
+                <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+            </Breadcrumb>
+            <div className="col-12">
+                <h3>{props.dish.name}</h3>
+                <hr />
+            </div>                
+        </div>
         <div className="row">
             <div className="col-12 col-md-5 m-1">
-                {
-                RenderDish(props)
-            } </div>
+                <RenderDish dish={props.dish} />
+            </div>
             <div className="col-12 col-md-5 m-1">
-
-                {
-                RenderCommentContainer(props)
-            } </div>
+                <RenderComments comments={props.comments} />
+            </div>
         </div>
-    )
+        </div>
+    );
 
 
 }
--- Web-Technologies/Full-Stack-Web-Development-with-React-Specialization/Front-End-Web-Development-with-React/1.project/confusion/src/components/MainComponent.js
+++ Web-Technologies/Full-Stack-Web-Development-with-React-Specialization/Front-End-Web-Development-with-React/1.project/confusion/src/components/MainComponent.js
@@ -1,11 +1,6 @@
 import React, { Component } from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
 
-
-
-import { Navbar, NavbarBrand } from 'reactstrap';
-
-
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 
@@ -38,6 +33,8 @@ class Main extends Component {
     this.setState({ selectedDish: dishId});
   }
 
+  
+
   render() {
     const HomePage = () => {
       return(
@@ -48,6 +45,13 @@ class Main extends Component {
           />
       );
     }
+
+    const DishWithId = ({match}) => {
+      return(
+          <DishDetail dish={this.state.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]} 
+            comments={this.state.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))} />
+      );
+    };
     return (
       <div>
        
@@ -55,12 +59,11 @@ class Main extends Component {
         <Switch>
               <Route path='/home' component={HomePage} />
               <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
+              <Route path='/menu/:dishId' component={DishWithId} />
               <Route exact path='/contactus' component={Contact} />} />
-
               <Redirect to="/home" />
           </Switch>
        
-        <DishDetail dish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]} />
         <Footer />
       </div>
     );
