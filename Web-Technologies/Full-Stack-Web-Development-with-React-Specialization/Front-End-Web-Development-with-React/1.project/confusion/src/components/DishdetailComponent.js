cf5ecd4a87b8604dfba8679f974b84eed4ccc0e4
danielphilipjohnson <daniel-philip-johnson@outlook.com>
2020-07-13 21:16:41 +0100

    Redux Actions

--- Web-Technologies/Full-Stack-Web-Development-with-React-Specialization/Front-End-Web-Development-with-React/1.project/confusion/src/components/DishdetailComponent.js
+++ Web-Technologies/Full-Stack-Web-Development-with-React-Specialization/Front-End-Web-Development-with-React/1.project/confusion/src/components/DishdetailComponent.js
@@ -17,7 +17,7 @@ import {
 } from 'reactstrap';
 //import CommentForm from './CommentForm';
 import { Link } from 'react-router-dom';
-import {Control, LocalForm, Errors} from 'react-redux-form';
+import { Control, LocalForm, Errors } from 'react-redux-form';
 
 
 
@@ -38,7 +38,7 @@ class CommentForm extends Component {
             author: '',
             rating: '',
             comments: ''
-            
+
         };
 
         this.handleSubmit = this.handleSubmit.bind(this);
@@ -53,6 +53,9 @@ class CommentForm extends Component {
     }
 
     handleSubmit(values) {
+        this.toggleModal();
+        this.props.addComment(this.props.dishId, values.rating, values.author,
+            values.comment);
         console.log('Current State is: ' + JSON.stringify(values));
         alert('Current State is: ' + JSON.stringify(values));
 
@@ -69,11 +72,11 @@ class CommentForm extends Component {
         return (
             <div>
                 <Modal isOpen={
-                        this.state.isModalOpen
-                    }
+                    this.state.isModalOpen
+                }
                     toggle={
                         this.toggleModal
-                }>
+                    }>
                     <ModalHeader toggle={
                         this.toggleModal
                     }>Submit Comment
@@ -101,9 +104,9 @@ class CommentForm extends Component {
                                     md={12}>Your Name</Label>
                                 <Col md={12}>
                                     <Control.text model=".author" id="author" name="author" placeholder="Your Name" className="form-control"
-                                    validators={{
-                                       minLength: minLength(3), maxLength: maxLength(15)
-                                    }}
+                                        validators={{
+                                            minLength: minLength(3), maxLength: maxLength(15)
+                                        }}
                                     />
                                     <Errors
                                         className="text-danger"
@@ -114,7 +117,7 @@ class CommentForm extends Component {
                                             minLength: 'Must be greater than 2 characters',
                                             maxLength: 'Must be 15 characters or less'
                                         }}
-                                     />
+                                    />
                                 </Col>
                             </Row>
 
@@ -122,15 +125,11 @@ class CommentForm extends Component {
                                 <Label htmlFor="comment"
                                     md={12}>Comment</Label>
                                 <Col md={12}>
-                                    <Control.textarea model=".comment" id="comment" name="comment" rows="8" className="form-control"/>
+                                    <Control.textarea model=".comment" id="comment" name="comment" rows="8" className="form-control" />
                                 </Col>
                             </Row>
                             <Row className="form-group">
-                                <Col md={
-                                    {
-                                        size: 10,
-                                    }
-                                }>
+                                <Col md={{ size: 10 }}>
                                     <Button type="submit" color="primary">
                                         Submit
                                     </Button>
@@ -142,7 +141,7 @@ class CommentForm extends Component {
                 <Button outline
                     onClick={
                         this.toggleModal
-                }>
+                    }>
 
                     <span className="fa fa-pencil fa-lg"></span>
                     Submit Comment
@@ -159,6 +158,7 @@ class CommentForm extends Component {
 function RenderDish({ dish }) {
     if (dish != null) {
         return (
+            <div className="col-12 col-md-5 m-1">
             <Card>
                 <CardImg top
                     src={
@@ -176,6 +176,7 @@ function RenderDish({ dish }) {
                     } </CardText>
                 </CardBody>
             </Card>
+            </div>
         );
 
 
@@ -187,27 +188,38 @@ function RenderDish({ dish }) {
 };
 
 
-function RenderComments({ comments }) {
-    return (comments.map((comment) => {
-        return (
+function RenderComments({ comments, addComment, dishId }) {
 
-            <ul className="list-unstyled">
-                <li>{
-                    comment.comment
-                }</li>
-                <li>-- {
-                    comment.author
-                }
-                    , {
-                        new Intl.DateTimeFormat('en-US', {
-                            year: 'numeric',
-                            month: 'short',
-                            day: '2-digit'
-                        }).format(new Date(Date.parse(comment.date)))
-                    }</li>
-            </ul>
+        return (
+            <div className="col-12 col-md-5 m-1">
+                <h4>Comments</h4>
+                <ul className="list-unstyled">
+                    {
+                        comments.map((comment) => {
+
+                            return (
+
+                                <li key={comment.id}>
+                                    <p>{
+                                        comment.comment
+                                    }</p>
+                                    <p>
+                                        -- {comment.author}  , {
+                                            new Intl.DateTimeFormat('en-US', {
+                                                year: 'numeric',
+                                                month: 'short',
+                                                day: '2-digit'
+                                            }).format(new Date(Date.parse(comment.date)))
+                                        }
+                                    </p>
+                                </li>
+
+                            )
+                        })}
+                </ul>
+                <CommentForm dishId={dishId} addComment={addComment} />
+            </div>
         );
-    }));
 
 }
 
@@ -215,13 +227,13 @@ function RenderComments({ comments }) {
 
 const DishDetail = (props) => {
 
-   
-    
-    
+
+
+
     return (
         <div className="container">
             <div className="row">
-               
+
                 <Breadcrumb>
 
                     <BreadcrumbItem>
@@ -240,17 +252,20 @@ const DishDetail = (props) => {
                 </div>
             </div>
             <div className="row">
-                <div className="col-12 col-md-5 m-1">
-                    <RenderDish dish={
-                        props.dish
-                    } />
-                </div>
-                <div className="col-12 col-md-5 m-1">
-                    <RenderComments comments={
+
+                <RenderDish dish={
+                    props.dish
+                } />
+
+                <RenderComments
+                    comments={
                         props.comments
-                    } />
-                     <CommentForm />
-                </div>
+                    }
+                    addComment={props.addComment}
+                    dishId={props.dish.id}
+                />
+
+
             </div>
         </div>
     );
--- Web-Technologies/Full-Stack-Web-Development-with-React-Specialization/Front-End-Web-Development-with-React/1.project/confusion/src/components/MainComponent.js
+++ Web-Technologies/Full-Stack-Web-Development-with-React-Specialization/Front-End-Web-Development-with-React/1.project/confusion/src/components/MainComponent.js
@@ -19,6 +19,14 @@ import { COMMENTS } from '../shared/comments';
 import { PROMOTIONS } from '../shared/promotions';
 import { LEADERS } from '../shared/leaders';
 
+import { addComment } from '../redux/ActionCreators';
+
+
+const mapDispatchToProps = dispatch => ({
+
+    addComment: (dishId, rating, author, comment) => dispatch(addComment(dishId, rating, author, comment))
+
+});
 
 
 const mapStateToProps = state => {
@@ -62,8 +70,11 @@ class Main extends Component {
 
         const DishWithId = ({ match }) => {
             return (
+
                 <DishDetail dish={this.props.dishes.filter((dish) => dish.id === parseInt(match.params.dishId, 10))[0]}
-                    comments={this.props.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId, 10))} />
+                    comments={this.props.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId, 10))}
+                    addComment={this.props.addComment}
+                />
             );
         };
 
@@ -88,5 +99,5 @@ class Main extends Component {
     }
 }
 
-export default withRouter(connect(mapStateToProps)(Main));
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(Main));
 
