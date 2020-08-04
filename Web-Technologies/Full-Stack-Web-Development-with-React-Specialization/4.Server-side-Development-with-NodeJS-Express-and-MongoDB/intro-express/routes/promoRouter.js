062b45da593e3591ccc87f595b5279ab3014ce50
danielphilipjohnson <daniel-philip-johnson@outlook.com>
2020-08-04 12:38:43 +0100

    Completed Assignment 1

---
+++ Web-Technologies/Full-Stack-Web-Development-with-React-Specialization/4.Server-side-Development-with-NodeJS-Express-and-MongoDB/intro-express/routes/promoRouter.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const promoRouter = express.Router();
+
+promoRouter.use(bodyParser.json());
+
+promoRouter.route('/')
+.all((req,res,next) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    next();
+})
+.get((req,res,next) => {
+    res.end('Will send all the promos to you!');
+})
+.post((req, res, next) => {
+    res.end('Will add the promo: ' + req.body.name + ' with details: ' + req.body.description);
+})
+.put((req, res, next) => {
+    res.statusCode = 403;
+    res.end('PUT operation not supported on /promos');
+})
+.delete((req, res, next) => {
+    res.end('Deleting all promos');
+});
+
+promoRouter.route('/:promoId')
+.all((req,res,next) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    next();
+})
+.get((req,res,next) => {
+    console.log(req.params.promoId)
+    res.end('Will send you the promo ' + req.params.promoId + "!");
+})
+.post((req, res, next) => {
+    res.end('Will add the promo ' + req.params.promoId + ": "+ req.body.name + ' with details: ' + req.body.description);
+})
+.put((req, res, next) => {
+    res.statusCode = 403;
+    res.end('PUT operation not supported on /promo/ ' + req.params.promoId);
+})
+.delete((req, res, next) => {
+    res.end('Delete promo ' +req.params.promoId );
+});
+
+module.exports = promoRouter;
