38ec62b885c2fb5d987ec8858a7314d12c88f789
danielphilipjohnson <daniel-philip-johnson@outlook.com>
2020-08-04 13:09:18 +0100

    Express Generator REST API

---
+++ Web-Technologies/Full-Stack-Web-Development-with-React-Specialization/4.Server-side-Development-with-NodeJS-Express-and-MongoDB/2.week/conFusionServer/routes/leaderRouter.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const leaderRouter = express.Router();
+
+leaderRouter.use(bodyParser.json());
+
+leaderRouter.route('/')
+.all((req,res,next) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    next();
+})
+.get((req,res,next) => {
+    res.end('Will send all the leaders to you!');
+})
+.post((req, res, next) => {
+    res.end('Will add the leader: ' + req.body.name + ' with details: ' + req.body.description);
+})
+.put((req, res, next) => {
+    res.statusCode = 403;
+    res.end('PUT operation not supported on /leaders');
+})
+.delete((req, res, next) => {
+    res.end('Deleting all leaders');
+});
+
+leaderRouter.route('/:leaderId')
+.all((req,res,next) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    next();
+})
+.get((req,res,next) => {
+    console.log(req.params.leaderId)
+    res.end('Will send you the leader ' + req.params.leaderId + "!");
+})
+.post((req, res, next) => {
+    res.end('Will add the leader ' + req.params.leaderId + ": "+ req.body.name + ' with details: ' + req.body.description);
+})
+.put((req, res, next) => {
+    res.statusCode = 403;
+    res.end('PUT operation not supported on /leader/ ' + req.params.leaderId);
+})
+.delete((req, res, next) => {
+    res.end('Delete leader ' +req.params.leaderId );
+});
+
+module.exports = leaderRouter;
