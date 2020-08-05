a535a255c95a462c757d33ab8f7a69b9b0934b14
danielphilipjohnson <daniel-philip-johnson@outlook.com>
2020-08-05 11:12:25 +0100

    File Upload

---
+++ Web-Technologies/Full-Stack-Web-Development-with-React-Specialization/4.Server-side-Development-with-NodeJS-Express-and-MongoDB/4.week/conFusionServer/routes/leaderRouter.js
@@ -0,0 +1,81 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const leaderRouter = express.Router();
+const authenticate = require('../authenticate');
+const Leaders = require('../models/leaders');
+
+leaderRouter.use(bodyParser.json());
+
+leaderRouter.route('/')
+.get((req,res,next) => {
+    Leaders.find({})
+    .then((leaders) => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');    
+        res.json(leaders);
+    }, (err) => next(err))
+    .catch((err) => next(err));
+})
+.post(authenticate.verifyUser, (req, res, next) => {
+    Leaders.create(req.body)
+    .then((leader) => {
+        console.log('Leader Created ', leader);
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');    
+        res.json(leader);
+    }, (err) => next(err))
+    .catch((err) => next(err));
+})
+.put(authenticate.verifyUser, (req, res, next) => {
+    res.statusCode = 403;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('PUT operation not supported on /leaders');
+})
+.delete(authenticate.verifyUser, (req, res, next) => {
+    Leaders.deleteOne({})
+    .then((resp) => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');    
+        res.json(resp);
+    }, (err) => next(err))
+    .catch((err) => next(err));
+});
+
+leaderRouter.route('/:leaderId')
+.get((req,res,next) => {
+    Leaders.findById(req.params.leaderId)
+    .then((leader) => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');    
+        res.json(leader);
+    }, (err) => next(err))
+    .catch((err) => next(err));
+})
+.post(authenticate.verifyUser, (req, res, next) => {
+    res.statusCode = 403;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('POST operation not supported on /leaders/'+ req.params.leaderId);
+})
+.put(authenticate.verifyUser, (req, res, next) => {
+    Leaders.findByIdAndUpdate(req.params.leaderId, {
+        $set: req.body
+    }, { new: true })
+    .then((leader) => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');            
+        res.json(leader);
+    }, (err) => next(err))
+    .catch((err) => next(err));
+})
+.delete(authenticate.verifyUser, (req, res, next) => {
+    Leaders.findByIdAndRemove(req.params.leaderId)
+    .then((resp) => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');    
+        res.json(resp);
+    }, (err) => next(err))
+    .catch((err) => next(err));
+});
+
+module.exports = leaderRouter;
